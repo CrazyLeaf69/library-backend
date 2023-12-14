51f57f351ebb70cf99533cf143e68f42f0c86056
Lowe Löwing <lowe.lowing@gmail.com>
2023-12-14 15:19:48 +0100

    new hosting

--- Program.cs
+++ Program.cs
@@ -10,15 +10,15 @@ using Azure.Extensions.AspNetCore.Configuration.Secrets;
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager _configuration = builder.Configuration;
 
-string KVUrl = _configuration["KeyVaultConfig:KVUrl"];
-string tenantId = _configuration["KeyVaultConfig:TenantId"];
-string clientId = _configuration["KeyVaultConfig:ClientId"];
-string clientSecret = _configuration["KeyVaultConfig:ClientSecretId"];
+//string KVUrl = _configuration["KeyVaultConfig:KVUrl"];
+//string tenantId = _configuration["KeyVaultConfig:TenantId"];
+//string clientId = _configuration["KeyVaultConfig:ClientId"];
+//string clientSecret = _configuration["KeyVaultConfig:ClientSecretId"];
 
-var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+//var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
-var client = new SecretClient(new Uri(KVUrl), credential);
-builder.Configuration.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
+//var client = new SecretClient(new Uri(KVUrl), credential);
+//builder.Configuration.AddAzureKeyVault(client, new AzureKeyVaultConfigurationOptions());
 
 // Add services to the container.
 var services = builder.Services;
